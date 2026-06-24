9262865cc638d39ba3a274791bb6063e9a1b9f09
lizaberezhna <foxbetci@gmail.com>
2026-06-25 00:21:54 +0300

    Update main.js

--- 2/main.js
+++ 2/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const mindarThree = new MindARThree({
         container: document.body,
-        imageTargetSrc: "../assets/cats.mind",
+        imageTargetSrc: "../assets/card.mind",
     });
 
     const { renderer, scene, camera } = mindarThree;
@@ -14,19 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const cube = new THREE.Mesh(
         new THREE.BoxGeometry(0.5, 0.5, 0.5),
-        new THREE.MeshBasicMaterial({ color: "#7707d3" })
+        new THREE.MeshBasicMaterial({ color: "#914434" })
     );
     cube.position.set(-0.6, 0, 0);
 
     const sphere = new THREE.Mesh(
         new THREE.SphereGeometry(0.3, 32, 32),
-        new THREE.MeshBasicMaterial({ color: "#00ff95" })
+        new THREE.MeshBasicMaterial({ color: "#34914A" })
     );
     sphere.position.set(0.6, 0, 0);
 
     const cone = new THREE.Mesh(
         new THREE.ConeGeometry(0.3, 0.7, 32),
-        new THREE.MeshBasicMaterial({ color: "#ff0099" })
+        new THREE.MeshBasicMaterial({ color: "343C91" })
     );
     cone.position.set(0, 0.7, 0);
 
